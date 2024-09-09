d35523860b7a38b2aaa7dfe2aedcd6bcc877b585
稲葉正健 <inabamasatake@inabaseikennoMacBook-Air.local>
2024-09-09 10:11:09 +0900

    commit

---
+++ Desktop/FrontHacks/22ReactRouter/ex1_again/src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Button.css';
+
+
+const Button=(props)=>{
+    let{onClickHandler}=props;
+    //onClickHandlerに関数以外の値がセットされていたら
+    //ボタンクリック時にエラーを出さないために何も中身のない関数を実行する
+
+    if (typeof onClickHandler !=='function') {
+        onClickHandler=()=>{};
+    }
+
+    return (
+        <div className="Button" onClick={onClickHandler}>
+            {props.children}
+        </div>
+    );
+};  
+
+export default Button;
